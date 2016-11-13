3ec28842f47b72a08be6e6deaa745b8c6283316e
Tom Andrews <tom@andrewsmail.co.uk>
2016-11-13 09:42:09 +0000

    Use Recording not RecordingDetail

---
+++ Emby.MythTv/Model/Recording.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace babgvant.Emby.MythTv.Model
+{
+    public class Recording
+    {
+        public string RecordedId { get; set; }
+        public string Status { get; set; }
+        public string Priority { get; set; }
+        public DateTime StartTs { get; set; }
+        public DateTime EndTs { get; set; }
+        public string FileSize { get; set; }
+        public string FileName { get; set; }
+        public string HostName { get; set; }
+        public string LastModified { get; set; }
+        public string RecordId { get; set; }
+        public string RecGroup { get; set; }
+        public string PlayGroup { get; set; }
+        public string StorageGroup { get; set; }
+        public int RecType { get; set; }
+        public string DupInType { get; set; }
+        public string DupMethod { get; set; }
+        public string EncoderId { get; set; }
+        public string EncoderName { get; set; }
+        public string Profile { get; set; }
+    }
+}
