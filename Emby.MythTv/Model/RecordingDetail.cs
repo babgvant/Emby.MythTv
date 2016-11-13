542ac8758e2f3423535965dc8ddccc91ab4e901f
Tom Andrews <tom@andrewsmail.co.uk>
2016-11-13 09:42:09 +0000

    More fields in RecordingDetail

--- Emby.MythTv/Model/RecordingDetail.cs
+++ Emby.MythTv/Model/RecordingDetail.cs
@@ -8,18 +8,24 @@ namespace babgvant.Emby.MythTv.Model
 {
     public class RecordingDetail
     {
+        public string RecordedId { get; set; }
         public string Status { get; set; }
         public string Priority { get; set; }
-        public string StartTs { get; set; }
-        public string EndTs { get; set; }
+        public DateTime StartTs { get; set; }
+        public DateTime EndTs { get; set; }
+        public string FileSize { get; set; }
+        public string FileName { get; set; }
+        public string HostName { get; set; }
+        public string LastModified { get; set; }
         public string RecordId { get; set; }
         public string RecGroup { get; set; }
         public string PlayGroup { get; set; }
         public string StorageGroup { get; set; }
-        public string RecType { get; set; }
+        public int RecType { get; set; }
         public string DupInType { get; set; }
         public string DupMethod { get; set; }
         public string EncoderId { get; set; }
+        public string EncoderName { get; set; }
         public string Profile { get; set; }
     }
 }
